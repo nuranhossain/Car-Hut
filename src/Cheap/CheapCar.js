98000da7c5f44081281ddd88d72d055e95b9d2bf
unknown <nuranhossain@gmail.com>
2022-12-02 19:28:15 +0600

    assignment

--- src/Cheap/CheapCar.js
+++ src/Cheap/CheapCar.js
@@ -3,7 +3,7 @@ import { AuthContext } from "../Context/AuthProvider";
 import Modal from "../Modal/Modal";
 
 const CheapCar = ({ car, setSelectedCar }) => {
-  const { name, img, des, price, post, email, carName, location } = car;
+  const { name, img, des, price, post, email, carName, location, verify } = car;
   let { loading } = useContext(AuthContext);
   if (loading) {
     <h1 className="text-5xl text-center">Loading</h1>;
@@ -19,11 +19,29 @@ const CheapCar = ({ car, setSelectedCar }) => {
           <h2 className="card-title">{carName}</h2>
           <p>{des}</p>
           <h1 className="text-xl">${price}</h1>
-          <div className="user border rounded pl-3 py-2">
-            <span className="text small">Author: </span>{" "}
-            <span className="text-blue-400 font-bold">{name}</span>
-            <p>Email: {email}</p>
-            <p className="font-bold">Location: {location}</p>
+          <div className="user border rounded pl-3 py-2 shadow-lg">
+            <p className="text-center py-3 text-gray-600 font-bold">
+              Seller Info
+            </p>
+            <div className="flex justify-between">
+              <div>
+                <span className="text small">Author: </span>{" "}
+                <span className="text-blue-400 font-bold">{name}</span>
+              </div>
+              {verify && (
+                <div className="mr-2">
+                  {" "}
+                  <i class="fa-solid fa-check mr-2 text-orange-500"></i>
+                  <span>Verified</span>
+                </div>
+              )}
+            </div>
+            <p>
+              Email: <span className="font-bold">{email}</span>
+            </p>
+            <p>
+              Location: <span className="font-bold">{location}</span>
+            </p>
             <p>Date: {post}</p>
           </div>
           <div className="card-actions justify-end">
